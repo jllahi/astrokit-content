663b5ee4c0f28c09cfe1d8a3aab395146e1108e8
Jordi <jllahi@users.noreply.github.com>
2023-12-10 11:16:10 +0100

    Add Spotlight and Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sentry(), spotlightjs(), tailwind()]
+});
