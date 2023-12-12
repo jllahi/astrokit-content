603834de2d31ad6ee6a58b29a1067103222b2609
Jordi <jllahi@users.noreply.github.com>
2023-12-12 15:57:28 +0100

    Add expressive-code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
+// import mkcert from "vite-plugin-mkcert"
 import tailwind from "@astrojs/tailwind"
 import sentry from "@sentry/astro"
 import spotlightjs from "@spotlightjs/astro"
 import { defineConfig, squooshImageService } from "astro/config"
-import mkcert from "vite-plugin-mkcert"
+import astroExpressiveCode from "astro-expressive-code"
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [sentry(), spotlightjs(), tailwind()],
+	integrations: [sentry(), spotlightjs(), tailwind(), astroExpressiveCode()],
 	image: {
 		service: squooshImageService(),
 	},
