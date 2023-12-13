96d913b8e945b1b6fe67140c74d3224141cd4456
Jordi <jllahi@users.noreply.github.com>
2023-12-13 11:05:11 +0100

    Refactor code

--- src/utils/getSortedPosts.ts
+++ src/utils/getSortedPosts.ts
@@ -1,3 +1,9 @@
+/**
+ * Filters and sorts a collection of posts by date, excluding drafts.
+ *
+ * @param posts - The array of post entries to filter and sort.
+ * @returns The filtered and sorted array of post entries.
+ */
 import type { CollectionEntry } from "astro:content"
 
 const getSortedPosts = (posts: CollectionEntry<"posts">[]) =>
