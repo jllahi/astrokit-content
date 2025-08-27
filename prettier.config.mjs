b9d85a0de488ce672d2b561101b4406a8814654f
jllahi <1439075+jllahi@users.noreply.github.com>
2025-08-27 04:50:11 +0200

    chore: migrate prettier config from .prettierrc.js to prettier.config.mjs
    Update prettier configuration file format and adjust some formatting options

---
+++ prettier.config.mjs
@@ -0,0 +1,36 @@
+/** @type {import('prettier').Config} */
+
+const prettierConfig = {
+  arrowParens: 'always',
+  astroAllowShorthand: true,
+  bracketSameLine: true,
+  bracketSpacing: true,
+  endOfLine: 'lf', // end_of_line = lf
+  jsxSingleQuote: false,
+  printWidth: 100, // default: 80
+  quoteProps: 'consistent',
+  semi: false, // default: true
+  singleQuote: true, // default: false
+  tabWidth: 2, // indent_size = 2
+  tailwindAttributes: ['class:list', 'className'],
+  tailwindFunctions: ['clsx', 'cn', 'tw', 'tv'],
+  trailingComma: 'es5',
+  useTabs: false, // indent_style = space
+  // astroOrganizeImportsMode: 'All',
+  plugins: [
+    'prettier-plugin-astro',
+    'prettier-plugin-tailwindcss',
+    'prettier-plugin-packagejson',
+    'prettier-plugin-organize-imports',
+  ],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+  singleAttributePerLine: true,
+}
+export default prettierConfig
